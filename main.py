ad5e1c28781c55798f830d85cbabdd5efcfbf555
pakoec <francisco@rintin.co>
2024-01-25 16:59:01 -0500

    update main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from db.db_UserInteractionEvents import event_instert
 st.set_page_config(
         page_title="Rintin",
 )
-import login,picking, recoleccion, ingresoOrdenesCompra , auditoria,logout, cookiesMenu, register,test, register,agrupacion, ordenesCompra
+import login,picking,confirmacion, empaquetado, recoleccion, ingresoOrdenesCompra , auditoria,logout, register,test, register,agrupacion, ordenesCompra
 
 @st.cache_resource(experimental_allow_widgets=True)
 def get_manager():
@@ -40,23 +40,31 @@ class MultiApp:
             valEmail=cookie_manager.get(cookie="useremail")
             st.session_state.username=val
             st.session_state.useremail=valEmail
-            if val in ('Usuario Pickeo','Usuario_Pickeo2' ,'Usuario_Pickeo3','Usuario_Pickeo4'): 
-                menu=['Logout','Pickeo', 'Ingreso OC Bodega']
-                pagina=1
-            elif val in ('picker_oaxaca', 'ivan'):
+            # if val in ('Usuario Pickeo','Usuario_Pickeo2' ,'Usuario_Pickeo3','Usuario_Pickeo4'): 
+            #     menu=['Logout','Pickeo', 'Ingreso OC Bodega']
+            #     pagina=1
+            if val == 'picker_oaxaca':
                 menu=['Logout','Pickeo']
                 pagina=1
-            elif val == 'Usuario Recoleccion':
-                menu=['Logout','Recoleccion']
+            # elif val == 'Usuario Recoleccion':
+            #     menu=['Logout','Recoleccion']
+            #     pagina=1
+            elif val == 'aurea':
+                menu=['Logout','Confirmación Seller']
                 pagina=1
-            elif val in ('operaciones','santiago','leslie','joshua'):
-                menu=['Logout','Pickeo','Recoleccion','Auditoria','Agrupacion','Ordenes de Compra', 'Ingreso OC Bodega']
+            elif val in ('operaciones','santiago','leslie','joshua','jesus','morris','emilio','lucero','daniel','oscar','jonathan','ismael','ayjpickeo',):
+                print('entro')
+                print(val)
+                menu=['Logout','Agrupacion','Confirmación Seller', 'Empaquetado','Pickeo','Recoleccion','Auditoria','Agrupacion','Ordenes de Compra', 'Ingreso OC Bodega']
                 pagina=1
-            elif val == 'ismael':
-                menu=['Logout','Auditoria','Agrupacion']
+            elif val == ' ivan':
+                 menu=['Logout','Pickeo','Ordenes de Compra']
             elif val in ('francisco', 'JuanMa'):
-                menu=['Logout','Register','Pickeo','Recoleccion','Auditoria', 'Agrupacion','Ordenes de Compra', 'Ingreso OC Bodega','Cookies','Test']
+                menu=['Logout','Register','Pickeo','Confirmación Seller','Recoleccion','Auditoria', 'Agrupacion', 'Empaquetado','Ordenes de Compra', 'Ingreso OC Bodega']
             else:
+                print('entro')
+                print(val)
+                print('entro')
                 menu=['Login']
             app = option_menu(
                 menu_title='Operaciones',
@@ -80,6 +88,16 @@ class MultiApp:
             if valEmail is not None:
                 EventName,EventAction,EventUser='Main','acceso a la opción picking',valEmail
                 event_instert(EventName,EventAction,EventUser)
+        if app == 'Empaquetado':
+            empaquetado.app()
+            if valEmail is not None:
+                EventName,EventAction,EventUser='Main','acceso a la opción empaquetado',valEmail
+                event_instert(EventName,EventAction,EventUser)
+        if app == 'Confirmación Seller':
+            confirmacion.app()
+            if valEmail is not None:
+                EventName,EventAction,EventUser='Main','acceso a la opción confirmacion',valEmail
+                event_instert(EventName,EventAction,EventUser)
         if app == "Recoleccion":
             recoleccion.app()
             if valEmail is not None:
@@ -95,11 +113,11 @@ class MultiApp:
             if valEmail is not None:
                 EventName,EventAction,EventUser='Main','acceso a la opción ingresoPickeoReco',valEmail
                 event_instert(EventName,EventAction,EventUser)
-        if app == "Cookies":
-            cookiesMenu.app()
-            if valEmail is not None:
-                EventName,EventAction,EventUser='Main','acceso a la opción cookies',valEmail
-                event_instert(EventName,EventAction,EventUser)
+        #if app == "Cookies":
+        #    cookiesMenu.app()
+        #    if valEmail is not None:
+        #        EventName,EventAction,EventUser='Main','acceso a la opción cookies',valEmail
+        #        event_instert(EventName,EventAction,EventUser)
         if app == "Register":
             register.app()
             if valEmail is not None:
