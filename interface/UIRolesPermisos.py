3ba1496fa22262297e4e4f70bfc0b3eef8f04731
RintinMx <admin@rintin.co>
2024-02-07 12:42:27 -0500

    correcion de version de pandas
    version de pandas incompatible con version actual

--- interface/UIRolesPermisos.py
+++ interface/UIRolesPermisos.py
@@ -4,10 +4,11 @@ from db.db_roles import obtener_todos_los_roles
 from db.db_pemrisos import obtener_todos_los_permisos
 from streamlit_extras.dataframe_explorer import dataframe_explorer
 from db.db_rolesPermisos import insertar_rol_permiso, eliminar_rol_permiso, actualizar_rol_permiso, obtener_todos_los_roles_permisos
-from st_aggrid import AgGrid
+
 
 def anadir_fila(rol,permiso):
-    st.session_state.tabla = st.session_state.tabla.append({'Rol': rol, 'Permiso': permiso}, ignore_index=True)
+    st.session_state.tabla = pd.concat([st.session_state.tabla, pd.DataFrame([{'Rol': rol, 'Permiso': permiso}])], ignore_index=True)
+    #st.session_state.tabla = st.session_state.tabla.append({'Rol': rol, 'Permiso': permiso}, ignore_index=True)
     #st.experimental_rerun()
 
 # Función para eliminar una fila de la tabla
