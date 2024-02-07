8c4a1b573feda874ed84bd429afa7c9c9104a4ab
juangomez0120 <jmgomezortiz@gmail.com>
2024-02-07 11:15:11 -0600

    Ajuste ingreso pickup

--- ingresoPickup.py
+++ ingresoPickup.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from db.db_ingreso_pickup import get_seller_centro_padre
+from db.db_ingreso_pickup import get_orders
 from interface.UIingreso_pickup import ingresoPickup, finalizarProceso
 
 def app():
@@ -10,8 +10,10 @@ def app():
         if st.session_state.current_view in('final_proceso_picking_pickups','picking_pickups_detalle','picking_pickups', 'generar_guias', 'generar_guias_final','confirmacion','detalleEmpaquetado','finalProcesoEmpaquetado','detalleConfirmacion','finalProcesoConfirmacion','empaquetado', 'detalleAuditoria','finalProceso', 'detalleAgrupacion', 'finalProcesoAgrupacion','pick','recolect','detalle','recoleccion','pendiente','ordenesAgrupar','agrupacion','ordenesCompraMenu', 'editOrdenesCompra', 'terminar_orden_compra', 'ordenesCompra','auditoria', 'detalleOrdenCompra', 'ingresoOrdenesCompra', 'ordenesCompraCsv', 'ingresoOrdenesDetalle'):
             st.session_state['current_view'] = 'ingresoPickup'
         if st.session_state['current_view'] == 'ingresoPickup':
-            orders = get_seller_centro_padre()
-            ingresoPickup(orders)
+            orders = get_orders()
+            orders_dict = orders.to_dict(orient='list')
+            orders_for_filter = set(orders_dict['order_id'] + orders_dict['post_parent'])
+            ingresoPickup(orders, orders_for_filter)
         elif st.session_state['current_view'] == 'ingresoPickupFinal':
             parentId = st.session_state.parentId
             childList = st.session_state.orderList
