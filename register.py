5e78f07c9746566ab8c9284c8b41a89d53b1525f
RintinMx <admin@rintin.co>
2024-02-07 17:44:25 -0500

    update premisos
    actualización critica

--- register.py
+++ register.py
@@ -1,4 +1,27 @@
+import streamlit as st
 from interface.UIRegister import UIRegister
+from interface.UIRoles import roles
+from interface.UIOpcionesSistema import opcionSistema
+from interface.UIPermisos import permisos
+from interface.UIRolesPermisos import rolespermisos
+from interface.UIUsuario import users
+from db.db_roles import obtener_todos_los_roles
 
 def app():
-    UIRegister()
+
+    tab1, tab2, tab3,tab4,tab5,tab6 = st.tabs(["Todos los usuarios","Registro de usuario", "Roles",  "Permisos","Opciones del Sistema","Roles y Permisos"])
+    with tab1:
+        users()
+    with tab2:
+        st.header("Registro de usuario")
+        UIRegister()
+    with tab3:
+        roles()
+
+    with tab4:
+        opcionSistema()
+    with tab5:
+        permisos()
+    with tab6:
+        rolespermisos()
+       
