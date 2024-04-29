d0a97c5b8383f0d170c720fc86e341743e3af27b
juangomez0120 <jmgomezortiz@gmail.com>
2024-04-29 13:18:15 -0600

    Update ui_entrega_pedidos_pickup.py

--- interface/ui_entrega_pedidos_pickup.py
+++ interface/ui_entrega_pedidos_pickup.py
@@ -362,7 +362,7 @@ def ui_descargar_bitacora(data_general, data_detalle, fees):
             pdf.cell(20, 5, f"{str(data_detalle['estado'][i])}", align="C")
             pdf.cell(20, 5, f"{str(data_detalle['suborder'][i])}", align="C")
             pdf.cell(25, 5, f"{str(data_detalle['shop'][i])}", align="C")
-            pdf.cell(60, 5, f"{str(data_detalle['product_name'][i])[0:28]}", align="C")
+            pdf.cell(60, 5, f"{str(data_detalle['product_name'][i])[0:28].encode('latin-1', 'replace').decode('latin-1')}", align="C")
             y_2 = pdf.get_y()
             # reposition of cursor to write after a multicell
             pdf.set_xy(x + 125, y)
