5e4fb2603ed60093e1222fcd025bfaeb3b2a3127
juangomez0120 <jmgomezortiz@gmail.com>
2024-07-04 10:46:15 -0600

    remove whapi message

--- interface/ui_recoleccion.py
+++ interface/ui_recoleccion.py
@@ -12,13 +12,6 @@ from db.db_recoleccion import get_substitute_prod
 import streamlit_shadcn_ui as ui
 import asyncio
 
-from integration.endpoint_whapi import send_post_request_to_api
-
-async def send_whapi_message(wa_group_id, message):
-    result = await send_post_request_to_api(wa_group_id, message)
-    return result
-
-
 
 def UIpendienteRecoleccion(total_pedidos, total_paquetes , total_registros, data):
 
@@ -99,8 +92,6 @@ def UIpendienteRecoleccionSeleccion(total_pedidos, total_paquetes , total_regist
             if st.button("Recolectar", key=f"recolectar_{index}"):
                 EventName,EventAction,EventUser='picking','Se pulso en botón Recolectar',st.session_state.useremail
                 event_instert(EventName,EventAction,EventUser)
-                print(vendedor["wa_group_id"])
-                send_post_request_to_api(vendedor["wa_group_id"], "¡Nuestro recolector ya esta saliendo para tu tienda!")
                 st.session_state.Seller_name = vendedor["seller"]
                 print("st.session_state.Seller_name")
                 print(st.session_state.Seller_name)
